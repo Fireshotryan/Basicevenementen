f15697effcf7e1fd793a1a80686b31cd17991fa7
Ryan de Man <Ryan.m.de.man@gmail.com>
2023-01-17 09:02:46 +0100

    ja

--- Evenementen/create.php
+++ Evenementen/create.php
@@ -156,7 +156,7 @@ if (isset($_POST['submit'])) {
 
                 <div class="bg-gray-800 pt-3">
                     <div class="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white">
-                        <h1 class="font-bold pl-2">creëren evenement</h1>
+                        <h1 class="font-bold pl-2">creëer evenement</h1>
                     </div>
                 </div>
 
